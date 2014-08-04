032b8abf28de46c6c5fe0c55af18487647dc102e
Saurabh B <sbansod@iitk.ac.in>
2014-08-04 16:15:01 +0530

    Update production.rb for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,6 @@ Apps::Application.configure do
 
  # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
   
 end
